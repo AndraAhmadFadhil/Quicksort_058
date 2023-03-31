2569443ef581791cdadcfa6c92dc51243819db94
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2023-03-31 16:55:07 +0700

    membuat prosedur input

--- QuickSort_058/QuickSort_058.cpp
+++ QuickSort_058/QuickSort_058.cpp
@@ -3,3 +3,28 @@ using namespace std;
 
 int arr[20];
 int cmp_count = 0;
+int mov_count = 0;
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array :";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 0" << endl;
+	}
+
+	cout << "\n------------------" << endl;
+	cout << "\nEnter Array Elment" << endl;
+	cout << "\n------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
